d234d0764327893a542de6ab8639ea0519eed781
root <stenkinevgeniy@gmail.com>
2017-09-26 16:16:48 +0300

    1. Fix DTB header for A310f - this fix poweroff 2. Revert compiling dtb from sources 3. Add some blobs from stock

--- board/boot.mk
+++ board/boot.mk
@@ -9,13 +9,11 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 
 ### Boot.img configuration
-#BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk
-#BOARD_KERNEL_SEPARATED_DT := true
-BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt $(DEVICE_PATH)/dtb.img
-#TARGET_CUSTOM_DTBTOOL := dtbhtoolExynos
+BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk
+BOARD_KERNEL_SEPARATED_DT := true
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --board SRPOJ08A000KU
+TARGET_CUSTOM_DTBTOOL := dtbhtoolExynos
 BOARD_KERNEL_PAGESIZE := 2048
 #BOARD_KERNEL_CMDLINE := The bootloader ignores the cmdline from the boot.img
-#BOARD_KERNEL_SEPARATED_DT := true
-#TARGET_CUSTOM_DTBTOOL := dtbhtoolExynos
 TARGET_USES_UNCOMPRESSED_KERNEL := true
 
--- include/samsung_dtbh.h
+++ include/samsung_dtbh.h
@@ -24,8 +24,9 @@
 #define DTBH_SUBTYPE       "samsung"
 
  /* Hardcoded entry */
-#define DTBH_PLATFORM_CODE 0x347e
-#define DTBH_SUBTYPE_CODE  0x88668650
+#define DTBH_PLATFORM_CODE 0x50a6
+#define DTBH_SUBTYPE_CODE  0x217584da
+
 
 /* DTBH_MAGIC + DTBH_VERSION + DTB counts */
 #define DT_HEADER_PHYS_SIZE 12
