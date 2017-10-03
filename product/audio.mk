119af20362d599250b1279561bd1ee193184fa9c
root <stenkinevgeniy@gmail.com>
2017-10-03 11:34:36 +0300

    1. Fix root init srcipts. 2. Remove tinyalsa from audio.mk

--- product/audio.mk
+++ product/audio.mk
@@ -5,3 +5,7 @@
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+
+PRODUCT_PACKAGES += \
+    libtinyalsa
+
