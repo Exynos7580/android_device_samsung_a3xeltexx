5955932cc6a1b1eb49890db4a4de66e8a3dd8c1d
root <stenkinevgeniy@gmail.com>
2017-10-17 09:52:31 +0300

    Disable lineageos audio hal - very stupid hal :) Some times no voice sound, some times mic not working.. bugged
    Use again a3xeltexx audio hal.

--- product/audio.mk
+++ product/audio.mk
@@ -7,4 +7,4 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
     $(LOCAL_PATH)/configs/audio/mixer_paths_0.xml:system/etc/mixer_paths_0.xml
 
-#TARGET_AUDIOHAL_VARIANT=samsung
+TARGET_AUDIOHAL_VARIANT=a3xeltexx
