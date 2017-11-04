187a072a223d4ef5fd2b8d102c407074a894c6dd
root <stenkinevgeniy@gmail.com>
2017-11-04 10:22:12 +0300

    Revert old values

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -19,11 +19,11 @@
 #define _BDROID_BUILDCFG_H
 
 #define BTM_DEF_LOCAL_NAME   "a310f"
-//#define BTM_WBS_INCLUDED TRUE       /* Enable WBS */
-//#define BTIF_HF_WBS_PREFERRED TRUE  /* Use WBS    */
+#define BTM_WBS_INCLUDED TRUE       /* Enable WBS */
+#define BTIF_HF_WBS_PREFERRED TRUE  /* Use WBS    */
 // Networking, Capturing, Object Transfer
 // MAJOR CLASS: COMPUTER
 // MINOR CLASS: LAPTOP
-#define BTA_DISABLE_DELAY 1000
+//#define BTA_DISABLE_DELAY 1000
 
 #endif
