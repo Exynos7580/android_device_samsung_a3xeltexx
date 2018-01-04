35bed7958b79b0d1d5e0a415034eeec48d904be5
Victor Lourme <victorlourme@vlour.me>
2018-01-04 17:22:28 +0100

    a3xeltexx/kernel: remove s5neo dt.img * As i used S5neo to rebase device, we don't need the dt.img (a3xelte kernel have a stable dtbs)

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,6 +1,5 @@
 # Kernel
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --board SYSMAGIC000KU
-#BOARD_KERNEL_CMDLINE := The bootloader ignores the cmdline from the boot.img
 TARGET_KERNEL_CONFIG := lineage-a3xeltexx_defconfig
 TARGET_KERNEL_SOURCE := kernel/samsung/a3xeltexx
 BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk
