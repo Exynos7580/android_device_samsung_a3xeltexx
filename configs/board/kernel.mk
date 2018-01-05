f911587a57b7f669f6e71f2bec9d08e0f7350080
Victor Lourme <victorlourme@vlour.me>
2018-01-05 23:50:35 +0100

    a3xeltexx/kernel: switch back to common kernel
    Change-Id: I8c4e9db186c08d1c08dc154043e3bdd872f51245

--- configs/board/kernel.mk
+++ configs/board/kernel.mk
@@ -1,7 +1,7 @@
 # Kernel
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --board SYSMAGIC000KU
 TARGET_KERNEL_CONFIG := lineage-a3xeltexx_defconfig
-TARGET_KERNEL_SOURCE := kernel/samsung/a3xeltexx
+TARGET_KERNEL_SOURCE := kernel/samsung/exynos7580-common
 BOARD_CUSTOM_BOOTIMG_MK := hardware/samsung/mkbootimg.mk
 BOARD_KERNEL_IMAGE_NAME := Image
 TARGET_CUSTOM_DTBTOOL := dtbhtoolExynos
