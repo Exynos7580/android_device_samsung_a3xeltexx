2780e62ac329dbe757e7ae1469acd65971332ba1
Victor Lourme <victorlourme@vlour.me>
2018-01-11 18:45:42 +0100

    a3xeltexx: kernel: use recovered kernel * Device is finally booting using this kernel!
    Change-Id: I96f8d1a8ec51b32930d0294a8df669293856a8e6

--- configs/board/kernel.mk
+++ configs/board/kernel.mk
@@ -1,7 +1,7 @@
 # Kernel
 BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --dt $(DEVICE_PATH)/dt.img --board SRPOJ08A000KU
-TARGET_KERNEL_CONFIG := lineageos_a3xelte_defconfig
-TARGET_KERNEL_SOURCE := kernel/samsung/exynos7580-common
+TARGET_KERNEL_CONFIG := lineage-a3xeltexx_defconfig
+TARGET_KERNEL_SOURCE := kernel/samsung/a3xelte
 BOARD_CUSTOM_BOOTIMG_MK := device/samsung/a3xeltexx/mkbootimg.mk
 BOARD_KERNEL_IMAGE_NAME := Image
 TARGET_CUSTOM_DTBTOOL := dtbhtoolExynos
