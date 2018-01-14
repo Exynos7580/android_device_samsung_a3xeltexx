f52c293f27fcae4d95db486598438c09e0f9eb99
Victor Lourme <victorlourme@vlour.me>
2018-01-14 16:02:59 +0100

    a3xeltexx: use bcm2079x hidl
    Change-Id: I3d9020004872c8decfaa3298dc6c71be4e663337

--- configs/product/nfc.mk
+++ configs/product/nfc.mk
@@ -5,13 +5,10 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/nfc/nfcee_access.xml:system/etc/nfcee_access.xml
 
 PRODUCT_PACKAGES += \
-	libnfc-nci \
-	libnfc_nci_jni \
-	NfcNci \
-	Tag \
-	com.android.nfc_extras \
 	android.hardware.nfc@1.0-impl-bcm \
-	android.hardware.nfc@1.0-service
+	nfc_nci.bcm2079x.default \
+      	NfcNci \
+      	Tag
 
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.nfc.sec_hal=true
