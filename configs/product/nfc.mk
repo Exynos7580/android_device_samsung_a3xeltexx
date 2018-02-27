c1dd2c103cba1ac241cfc0d68aaca4cd4e7ce2f2
Victor Lourme <l0w@protonmail.ch>
2018-02-27 20:16:51 +0100

    a3xeltexx: [temp] don't build nfc
    * Disable NFC building while a fix is found

--- configs/product/nfc.mk
+++ configs/product/nfc.mk
@@ -1,10 +1,10 @@
 # NFC
-PRODUCT_COPY_FILES += \
+#PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/nfc/libnfc-sec-hal.conf:system/etc/libnfc-sec-hal.conf \
 	$(LOCAL_PATH)/configs/nfc/libnfc-sec.conf:system/etc/libnfc-brcm.conf \
 	$(LOCAL_PATH)/configs/nfc/nfcee_access.xml:system/etc/nfcee_access.xml
 
-PRODUCT_PACKAGES += \
+#PRODUCT_PACKAGES += \
 	libnfc \
     libnfc_jni \
     com.broadcom.nfc \
