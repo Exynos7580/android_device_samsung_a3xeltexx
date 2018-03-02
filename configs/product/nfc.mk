8cab076c399a08f03bc771515f3a7363e0536ebf
Victor Lourme <l0w@protonmail.ch>
2018-03-02 13:39:48 +0100

    a3xeltexx: fix NFC

--- configs/product/nfc.mk
+++ configs/product/nfc.mk
@@ -19,18 +19,15 @@
 # NFC
 #####################
 
-#PRODUCT_COPY_FILES += \
+PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/configs/nfc/libnfc-sec-hal.conf:system/etc/libnfc-sec-hal.conf \
 	$(LOCAL_PATH)/configs/nfc/libnfc-sec.conf:system/etc/libnfc-brcm.conf \
 	$(LOCAL_PATH)/configs/nfc/nfcee_access.xml:system/etc/nfcee_access.xml
 
-#PRODUCT_PACKAGES += \
-	libnfc \
-    libnfc_jni \
-    com.broadcom.nfc \
-    NfcNci \
-    Tag \
+PRODUCT_PACKAGES += \
+	libnfc-nci \
+	libnfc_nci_jni \
+	NfcNci \
+	Tag \
 	com.android.nfc_extras \
-	android.hardware.nfc@1.0-impl-bcm \
-	android.hardware.nfc@1.0-service
-
+	android.hardware.nfc@1.0-impl
