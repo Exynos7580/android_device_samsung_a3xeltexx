aa5966acc5499064c5e741650ecd52d6fcdf1166
Danny Wood <danwood76@gmail.com>
2018-08-29 19:10:32 +0100

    power: Add new HIDL based HAL to enable fine grained power management
    Change-Id: I2deb939e9179d734a883724e480788a0f33e9da9

--- configs/product/power.mk
+++ configs/product/power.mk
@@ -20,6 +20,7 @@
 #####################
 
 PRODUCT_PACKAGES += \
-	power.universal7580 \
-	android.hardware.power@1.0-service \
-	android.hardware.power@1.0-impl
+    android.hardware.power@1.0-service.universal7580
+
+PRODUCT_COPY_FILES += \
+    device/samsung/a3xeltexx/hardware/power/profiles.xml:system/etc/power_profiles.xml
